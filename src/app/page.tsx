a3a69237d424ec74346fd8784afd5799d317841a
Alvarlegur <34064054+Alvarlegur@users.noreply.github.com>
2025-01-10 16:46:23 +0000

    Create .gitignore

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import './page.css';
+
+export default function Home() {
+  return (
+    <div className="container">
+      <div className="text" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+
+        <h1>ALVARLEGUR</h1>
+      </div>
+      {/* <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        Alvarlegur ™
+      </footer> */}
+    </div>
+  );
+}
